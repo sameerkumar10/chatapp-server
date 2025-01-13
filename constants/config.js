880855c0a245d4142ddd2582646403687bf928d6
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2025-01-13 21:43:24 +0530

    Changes for error in routing on vercel 4

--- constants/config.js
+++ constants/config.js
@@ -1,16 +1,22 @@
 
-
-
 const corsOptions = {
-        origin: [
-          "http://localhost:5173",
-          "http://localhost:4173",
-          process.env.CLIENT_URL,
-        ],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-      
+  origin: (origin, callback) => {
+    const allowedOrigins = [
+      "http://localhost:5173",
+      "http://localhost:4173",
+      process.env.CLIENT_URL,
+    ];
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
+
 const SAMEER_TOKEN = "sameer-token";
 
-export {corsOptions,SAMEER_TOKEN};
+export { corsOptions, SAMEER_TOKEN };
