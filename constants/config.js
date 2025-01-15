825e072017bd5583170d1cb995d47ac3114e4b73
Sameer Kumar <103487647+sameerkumar10@users.noreply.github.com>
2025-01-15 23:04:48 +0530

    After deployment error solving 1

--- constants/config.js
+++ constants/config.js
@@ -5,10 +5,11 @@ const corsOptions = {
         origin: [
           "http://localhost:5173",
           "http://localhost:4173",
-          process.env.CLIENT_URL,
+          process.env.CLIENT_URL || "https://chatapp-frontend-mocha.vercel.app", 
         ],
-        methods: ["GET", "POST", "PUT", "DELETE"],
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
         credentials: true,
+        allowedHeaders: ["Content-Type", "Authorization"],
       
 };
 const SAMEER_TOKEN = "sameer-token";
