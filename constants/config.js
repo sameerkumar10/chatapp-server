336c34265e310cea28afc55d342ec571eb6ae365
sameerhashout <sameerkumar@hashouttech.com>
2025-12-23 01:08:10 +0530

    Fix CORS configuration for Vercel

--- constants/config.js
+++ constants/config.js
@@ -2,11 +2,13 @@
 
 
 const corsOptions = {
-        origin: [
-          "http://localhost:5173",
-          "http://localhost:4173",
-          process.env.CLIENT_URL,
-        ],
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:4173",
+    process.env.CLIENT_URL,
+    "https://chatapp-frontend-rouge.vercel.app",
+    "https://chatapp-frontend-nvbvomzfp-sameerkumar10-s-team.vercel.app",
+  ],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
       
