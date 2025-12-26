5e2effeaedf6725addec10b7aec7483f9caf8a71
sameerhashout <sameerkumar@hashouttech.com>
2025-12-26 13:26:20 +0530

    added some configuration

--- constants/config.js
+++ constants/config.js
@@ -4,6 +4,7 @@
 const corsOptions = {
   origin: [
     "http://localhost:5173",
+    "http://localhost:5174",
     "http://localhost:4173",
     process.env.CLIENT_URL,
     "https://chatapp-frontend-rouge.vercel.app",
